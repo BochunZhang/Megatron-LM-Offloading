db4089eb885c6e50c5e644e1f002479a59072f74
ZhangBochun <zhangbochun@ZhangBochundeMacBook-Pro.local>
2026-04-23 20:55:22 +0800

    add memory estimator

--- tools/analyse/estimator/moe_mem_estimator/layers.py
+++ tools/analyse/estimator/moe_mem_estimator/layers.py
@@ -624,9 +624,11 @@ class TEGroupedMLP(MemEstimator):
         ret = 0
 
         ret += self.linear_fc1.num_activation(input_shape)
+        
+        input_shape = deepcopy(input_shape)
+        input_shape[1] //= self.num_local_experts  # 外面已经 x topK 了, 这里应该均分给每个 expert, 但内部是完整的一个矩阵, 因此只能均分
         input_shape = self.linear_fc1.mock_forward(input_shape)
-
-        input_shape
+        
 
         # activation
         if not self.activation_recompute:
