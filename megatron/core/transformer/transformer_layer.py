f7607ceedb1dfba2f7782b08243ab05a1b60d876
BochunZhang <bzhangcv@connect.ust.hk>
2026-06-21 21:32:57 +0800

    add more fine-grained offload group to detect all activations

--- megatron/core/transformer/transformer_layer.py
+++ megatron/core/transformer/transformer_layer.py
@@ -632,10 +632,19 @@ class TransformerLayer(GraphableMegatronModule, BaseTransformerLayer):
             # self attention module.
             hidden_states = attention_output_with_bias[0]
         else:
-            with self.bias_dropout_add_exec_handler():
-                hidden_states = self.self_attn_bda(self.training, self.config.bias_dropout_fusion)(
-                    attention_output_with_bias, residual, self.hidden_dropout
-                )
+            # with self.bias_dropout_add_exec_handler():
+            #     hidden_states = self.self_attn_bda(self.training, self.config.bias_dropout_fusion)(
+            #         attention_output_with_bias, residual, self.hidden_dropout
+            #     )
+
+            with off_interface(True, attention_output_with_bias, "self_attn_bda") as attention_output_with_bias:
+                with self.bias_dropout_add_exec_handler():
+                    hidden_states = self.self_attn_bda(self.training, self.config.bias_dropout_fusion)(
+                        attention_output_with_bias, residual, self.hidden_dropout
+                    )
+        
+        off_interface.group_commit(hidden_states, name="self_attn_bda", forced_released_tensors=[residual])
+
         nvtx_range_pop(suffix="self_attn_bda")
 
         # Delay the offload of the attention norm until after the self_attn_bda has been computed
@@ -820,10 +829,16 @@ class TransformerLayer(GraphableMegatronModule, BaseTransformerLayer):
             # MLP module.
             hidden_states = mlp_output_with_bias[0]
         else:
-            with self.bias_dropout_add_exec_handler():
-                hidden_states = self.mlp_bda(self.training, self.config.bias_dropout_fusion)(
-                    mlp_output_with_bias, residual, self.hidden_dropout
-                )
+            # with self.bias_dropout_add_exec_handler():
+            #     hidden_states = self.mlp_bda(self.training, self.config.bias_dropout_fusion)(
+            #         mlp_output_with_bias, residual, self.hidden_dropout
+            #     )
+            with off_interface(True, mlp_output_with_bias, "mlp_bda") as mlp_output_with_bias:
+                with self.bias_dropout_add_exec_handler():
+                    hidden_states = self.mlp_bda(self.training, self.config.bias_dropout_fusion)(
+                        mlp_output_with_bias, residual, self.hidden_dropout
+                    )
+
         nvtx_range_pop(suffix="mlp_bda")
         # Delay the offload of the mlp norm until after the mlp_bda has been computed
         # because the residual is needed in the mlp_bda.
@@ -831,6 +846,10 @@ class TransformerLayer(GraphableMegatronModule, BaseTransformerLayer):
             hidden_states = off_interface.group_commit(
                 hidden_states, name="mlp_norm", forced_released_tensors=[residual]
             )
+
+        hidden_states = off_interface.group_commit(
+            hidden_states, name="mlp_bda", forced_released_tensors=[]
+        )
 
         # Jit compiled function creates 'view' tensor. This tensor
         # potentially gets saved in the MPU checkpoint function context,
